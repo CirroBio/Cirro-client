e9ff712143d480def579f100e6b999edfd1c84a8
Sam Minot <sminot@fredhutch.org>
2022-06-09 10:30:30 -0700

    WIP - saving outputs

--- pubweb/helpers/workflow_config.py
+++ pubweb/helpers/workflow_config.py
@@ -1,14 +1,64 @@
+import json
 from github import Github
-from questionary import prompt
+import os
+import questionary
 
 
 def prompt_wrapper(questions):
-    answers = prompt(questions)
+    answers = questionary.prompt(questions)
     # Prompt catches KeyboardInterrupt and sends back an empty dictionary
     # We want to catch this exception
     if len(answers) == 0:
         raise KeyboardInterrupt()
     return answers
+
+
+def type_validator(t, v):
+    """Return a boolean indicating whether `v` can be cast to `t(v)` without raising a ValueError."""
+    try:
+        t(v)
+        return True
+    except ValueError:
+        return False
+
+
+def ask(fname, msg, validate_type=None, output_f=None, **kwargs) -> str:
+    """Wrap questionary functions to catch escapes and exit gracefully."""
+
+    # Get the questionary function
+    questionary_f = questionary.__dict__.get(fname)
+
+    # Make sure that the function exists
+    assert questionary_f is not None, f"No such questionary function: {fname}"
+
+    if fname == "select":
+        kwargs["use_shortcuts"] = True
+
+    if validate_type is not None:
+        kwargs["validate"] = lambda v: type_validator(validate_type, v)
+
+    # The default value must be a string
+    if kwargs.get("default") is not None:
+        kwargs["default"] = str(kwargs["default"])
+
+    # Add a spacer line before asking the question
+    print("")
+
+    # Get the response
+    resp = questionary_f(msg, **kwargs).ask()
+
+    # If the user escaped the question
+    if resp is None:
+        raise KeyboardInterrupt()
+
+    # If an output transformation function was defined
+    if output_f is not None:
+
+        # Call the function
+        resp = output_f(resp)
+
+    # Otherwise
+    return resp
 
 
 class WorkflowConfig:
@@ -29,6 +79,18 @@ class WorkflowConfig:
             input=dict(),
             output=dict()
         )
+
+        # The compute configuration is boilerplate at this point
+        self.compute_config = """profiles {
+    standard {
+        process {
+            executor = 'awsbatch'
+            errorStrategy = 'retry'
+            maxRetries = 2
+        }
+    }
+}
+"""
 
     def configure(self):
         """Main method for getting user input, parsing the repo, and creating process docs."""
@@ -56,24 +118,87 @@ class WorkflowConfig:
     def save_local(self):
         """Write out the workflow configuration as a collection of files."""
 
-        pass
+        # Get the folder to use for outputs
+        output_folder = self._get_output_folder()
+
+        # Save each of the items in the process configuration
+        for k, v in self.process_config.items():
+
+            # Use the dictionary key to drive the file name
+            output_fp = f"process-{k}.json"
+            print(f"Writing out to {output_fp}")
+
+            # Open the file
+            with open(output_fp, "w") as handle:
+
+                # Serialize as JSON
+                json.dump(v, handle, indent=4)
+
+        # Write the compute configuration
+        output_fp = os.path.join(output_folder, "process-compute.config")
+
+        with open(output_fp, "w") as handle:
+            handle.write(self.compute_config)
+
+        print(f"Boilerplate compute configuration has been written to {output_fp} -- please modify that file as necessary.")
+
+        print(f"Done writing all process configuration items to {output_fp}")
+
+    def _get_output_folder(self):
+        """Get the output folder """
+
+        output_folder = ask(
+            "path",
+            "What folder should be used to write the workflow configuration?",
+            default="./",
+            only_directories=True
+        )
+
+        # If the path does not exist
+        if not os.path.exists(output_folder):
+
+            # Ask if it should be created
+            resp = ask(
+                "select",
+                f"The path does not exist: {output_folder}\nWould you like to create it, or pick another folder?",
+                choices=[
+                    "Create the folder",
+                    "Select another"
+                ]
+            )
+
+            if resp == "Create the folder":
+                os.makedirs(output_folder)
+                return output_folder
+
+            else:
+                return self._get_output_folder()
+
+        else:
+            return output_folder
 
     def _configure_repository(self):
         """Configure the workflow repository."""
 
+        # Set up the boilerplate elements of the dynamo record
+        self._add_dynamo_boilerplate()
+
         # Get the organization
-        org = prompt_wrapper({
-            'type': 'input',
-            'name': 'org',
-            'message': 'Which GitHub organization is the workflow located within?',
-            'default': 'nf-core'
-        })["org"]
+        org = ask(
+            'text',
+            'Which GitHub organization is the workflow located within?',
+            default='nf-core'
+        )
 
         # Get the repository
         repo = self._prompt_repository(org)
 
         # Get the version
         version = self._prompt_repository_version(org, repo)
+
+        # Set up the process name based on the workflow/version
+        process_id = f"process-{org}-{repo}-{version}"
+        self.process_config["dynamo"]["id"] = process_id
 
         # Get the entrypoint to use
         entrypoint = prompt_wrapper(dict(
@@ -100,11 +225,36 @@ class WorkflowConfig:
             version=version
         )
 
+        # Add any child processes that may exist
+        self.process_config["dynamo"]["childProcessIds"] = [
+            p.split("\n")[-1].strip(" ")
+            for p in ask(
+                "checkbox",
+                "Select any processes which can be run on the outputs of this workflow",
+                choices=[
+                    f"{process['name']}\n     {process['desc']}\n     {process['id']}"
+                    for process in self.client.process.list(process_type='NEXTFLOW')
+                ]
+            )
+        ]
+
+    def _add_dynamo_boilerplate(self):
+        """Add the elements of the dynamo record which do not vary by user entry."""
+
+        self.process_config["dynamo"]["executor"] = "NEXTFLOW"
+        self.process_config["dynamo"]["paramDefaults"] = []
+        self.process_config["dynamo"]["fileJson"] = ""
+        self.process_config["dynamo"]["componentJson"] = ""
+        self.process_config["dynamo"]["infoJson"] = ""
+
     def _prompt_repository(self, org):
         """Prompt the user for a repository contained within an organization."""
 
         # Get a list of repos in that organization
-        repo_list = [repo.name for repo in self.gh.get_user(org).get_repos()]
+        repo_list = [
+            repo.name for repo in self.gh.get_user(org).get_repos()
+            if repo.name != '.github'
+        ]
 
         # then use that to ask the user which repo to look at
         return prompt_wrapper({
