6a75d74a6ad957c840c642b0aa616d6737b9ef47
Nathan Thorpe <nathan@techstormpc.com>
2022-09-12 08:11:29 -0700

    add user agent header to api calls

--- pubweb/clients/api.py
+++ pubweb/clients/api.py
@@ -1,5 +1,7 @@
+import platform
 from typing import Dict
 
+import pkg_resources
 from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport
 
@@ -7,14 +9,20 @@ from pubweb import config
 from pubweb.auth.base import AuthInfo
 from pubweb.auth.iam import IAMAuth
 
-HEADERS = {
+try:
+    sdk_version = pkg_resources.get_distribution("pubweb").version
+except (pkg_resources.RequirementParseError, TypeError):
+    sdk_version = "Unknown"
+python_version = platform.python_version()
+headers = {
+    'User-Agent': f'PubWeb SDK {sdk_version} (Python {python_version})',
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
 
 
 def _build_gql_client(auth_info: AuthInfo, endpoint: str):
-    transport = RequestsHTTPTransport(url=endpoint, headers=HEADERS, auth=auth_info.get_request_auth())
+    transport = RequestsHTTPTransport(url=endpoint, headers=headers, auth=auth_info.get_request_auth())
     return Client(transport=transport, fetch_schema_from_transport=True)
 
 
