2f638c0d62b572b6c831206ebe0888ebf1e41d25
Nathan Thorpe <nathan@techstormpc.com>
2022-09-21 12:40:57 -0700

    bump versions

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ entry_points = {
 }
 
 install_requires = [
-    "click>=8.0,<8.1",
-    "boto3>=1.20,<1.30",
+    "click>=8.1.3",
+    "boto3>=1.24",
     "questionary==1.10.0",
-    "gql[requests]==3.0.0",
-    "requests==2.27.1",
+    "gql[requests]==3.4.0",
+    "requests==2.28.1",
     "requests_aws4auth==1.1.2",
-    "pycognito==2022.1.0",
+    "pycognito==2022.8.0",
     "tqdm==4.62.3",
     "pygithub>=1.55",
     "jsonschema==4.6.1"
@@ -26,7 +26,7 @@ install_requires = [
 
 install_analysis_requires = [
     "pandas>=1.4.0,<1.5.0",
-    "s3fs==2022.5.0"
+    "s3fs==2022.8.2"
 ]
 
 install_all_requires = install_requires + install_analysis_requires
