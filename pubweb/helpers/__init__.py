d807d3c4de3bf84c5ef149b5565ed31c42ca1c80
Sam Minot <sminot@fredhutch.org>
2022-10-05 16:33:01 -0700

    Proposal for how notebooks can be automated

--- pubweb/helpers/__init__.py
+++ pubweb/helpers/__init__.py
@@ -1,9 +1,11 @@
 from pubweb.helpers.workflow_config import WorkflowConfigBuilder
 from pubweb.helpers.preprocess_dataset import PreprocessDataset
 from pubweb.helpers.portal import DataPortal
+from pubweb.helpers.form import FormBuilder
 
 __all__ = [
     'WorkflowConfigBuilder',
     'PreprocessDataset',
-    'DataPortal'
+    'DataPortal',
+    'FormBuilder'
 ]
