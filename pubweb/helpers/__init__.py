6d73838b9db12a9b3a4992b87e3b6cd19a4c8c29
Sam Minot <sminot@fredhutch.org>
2022-10-31 15:32:11 -0700

    Move the DataPortal object to sdk/

--- pubweb/helpers/__init__.py
+++ pubweb/helpers/__init__.py
@@ -1,11 +1,9 @@
 from pubweb.helpers.workflow_config import WorkflowConfigBuilder
 from pubweb.helpers.preprocess_dataset import PreprocessDataset
-from pubweb.helpers.portal import DataPortal
 from pubweb.helpers.form import FormBuilder
 
 __all__ = [
     'WorkflowConfigBuilder',
     'PreprocessDataset',
-    'DataPortal',
     'FormBuilder'
 ]
