5bb6a3112a4333c7b82d6a1b6c8b34e425bcd118
Sam Minot <sminot@fredhutch.org>
2022-10-31 10:17:18 -0700

    Increment version (#44)

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_all_requires = install_requires + install_analysis_requires
 
 setup(
     name='pubweb',
-    version='0.3.8',
+    version='0.3.9',
     author='Fred Hutch',
     license='MIT',
     author_email='viz@fredhutch.org',
