a8ca096e69f775c1d39e8425c9dd83a38949f616
Sam Minot <sminot@fredhutch.org>
2022-11-02 15:48:27 -0700

    Pin botocore to allow aibotocore

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ entry_points = {
 install_requires = [
     "click>=8.1.3",
     "boto3>=1.25.5",
-    "botocore>=1.28.5",
+    "botocore==1.27.59",
     "questionary>=1.10.0",
     "gql[requests]>=3.4.0",
     "requests>=2.28.1",
