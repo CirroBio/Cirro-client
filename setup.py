fd91544cb44aa72bedaf6411e69c5941e8d48580
Sam Minot <sminot@fredhutch.org>
2022-11-02 15:54:48 -0700

    Pin versions which seem to work locally

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ entry_points = {
 
 install_requires = [
     "click>=8.1.3",
-    "boto3>=1.25.5",
-    "botocore==1.27.59",
+    "boto3==1.26.1",
+    "botocore==1.29.1",
     "questionary>=1.10.0",
     "gql[requests]>=3.4.0",
     "requests>=2.28.1",
@@ -24,8 +24,8 @@ install_requires = [
     "pandas>=1.5.0",
     "pygithub>=1.56",
     "jsonschema>=4.6.1",
-    "s3fs>=0.4.2",
-    "fsspec>=2022.10.0"
+    "s3fs==0.4.2",
+    "fsspec==2022.10.0"
 ]
 
 setup(
