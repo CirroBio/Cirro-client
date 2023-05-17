8406a88403d1005f1faaf0d4b5b83def2984c439
Nathan Thorpe <nathan@techstormpc.com>
2023-05-17 09:52:59 -0700

    cache --> save

--- cirro/cli/interactive/auth_args.py
+++ cirro/cli/interactive/auth_args.py
@@ -5,7 +5,7 @@ from cirro.cli.interactive.utils import ask_yes_no
 
 def gather_auth_config() -> Tuple[str, Dict]:
     auth_method_config = {
-        'enable_cache': ask_yes_no("Would you like to cache your login? (do not use this on shared devices)")
+        'enable_cache': ask_yes_no("Would you like to save your login? (do not use this on shared devices)")
     }
 
     return 'ClientAuth', auth_method_config
