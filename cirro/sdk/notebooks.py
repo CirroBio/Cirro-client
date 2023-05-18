8bb6753e43a3a93380e457100611872042a878ab
Sam Minot <sminot@fredhutch.org>
2023-05-18 11:48:30 -0700

    Expose parameters for notebooks

---
+++ cirro/sdk/notebooks.py
@@ -0,0 +1,189 @@
+from functools import lru_cache
+import json
+from pathlib import Path
+import re
+
+from typing import Any
+from cirro.sdk.exceptions import DataPortalAssetNotFound, DataPortalInputError
+
+
+def expose_param(
+    title: str,
+    default: Any,
+    description=None,
+    **kwargs
+):
+    """
+    Set up a variable parameter which can be specified by the user from
+    a web form for the purpose of running a notebook non-interactively.
+
+    Inputs:
+        title:          Name of the parameter
+        default:        Default value of the parameter
+        description:    (optional) Description of the parameter
+    
+    Any additional keyword arguments will be used as parameters of
+    the JSON schema which renders the webform.
+    """
+
+    # Check the type of the default value
+    param_type = _get_param_type(default)
+
+    # Format the ID of the element
+    id = _get_id(title)
+
+    # If there is a description
+    if description is not None:
+        # Then it must be a string
+        if not isinstance(description, str):
+            raise DataPortalInputError("Description must be a string")
+
+    # Check to see if there is a value provided in .cirro/params.json
+    # (which would indicate that this notebook is running in headless mode)
+    if _load_param(id) is not None:
+        
+        # Use the value from that file
+        return _load_param(id)
+    
+    # If no value is present, the notebook must be running interactively
+    else:
+
+        # Add the parameter definition to the .cirro/form.json
+        _save_param_schema(
+            title=title,
+            id=id,
+            type=param_type,
+            description=description,
+            default=default
+        )
+
+        # Return the default value
+        return default
+    
+
+def _save_param_schema(**param_schema):
+    """
+    Add the parameter schema to .cirro/form.json
+    """
+
+    # Load the existing schema
+    schema = FormSchema()
+
+    # Add the param
+    schema.add_param(**param_schema)
+
+    # Save the updated schema
+    schema.save()
+
+
+class FormSchema:
+    """Form schema object"""
+
+    def __init__(self, fp=".cirro/form.json"):
+        self._fp = fp
+
+        if Path(fp).exists():
+            self._schema = self._load_schema()
+        else:
+            self._schema = self._empty_schema()
+
+    def add_param(self, **param_schema):
+        """Add a param to the schema."""
+
+        # If the parameter already exists
+        saved_param = self._schema['form']['properties'].get(id)
+        if saved_param is not None:
+
+            # If any of the values are different
+            for kw, new_val in param_schema.items():
+                old_val = saved_param.get(kw)
+                # Warn the user
+                if old_val != new_val:
+                    print(f"Warning - changing {kw} of '{id}' from {old_val} to {new_val}")
+
+        # Set up the parameter definition
+        self._schema['form']['properties'][id] = param_schema
+
+    def save(self):
+        """Save the schema as JSON."""
+        with open(self._fp, 'w') as handle:
+            json.dump(self._schema, handle, indent=4)
+
+    def _load_schema(self) -> dict:
+        with open(self._fp, 'r') as handle:
+            schema: dict = json.load(handle)
+
+        if not isinstance(schema, dict):
+            msg = f"Invalid schema: {self._fp} - requires dict"
+            raise DataPortalAssetNotFound(msg)
+
+        for kw in ['form', 'ui']:
+            if schema.get(kw) is None:
+                msg = f"Invalid schema: {self._fp} - missing {kw}"
+                raise DataPortalAssetNotFound(msg)
+
+            if not isinstance(schema[kw], dict):
+                msg = f"Invalid schema: {self._fp} - {kw} should be dict"
+                raise DataPortalAssetNotFound(msg)
+            
+        return schema
+
+    def _empty_schema(self) -> dict:
+        return dict(
+            form=dict(
+                type='object',
+                properties=dict()
+            ),
+            ui=dict()
+        )
+
+
+@lru_cache
+def _load_param(id: str):
+    """Check to see if there is a value provided in .cirro/params.json."""
+
+    if Path('.cirro').exists() and Path('.cirro/params.json').exists():
+        with open('.cirro/params.json', 'r') as handle:
+            params = json.load(handle)
+            return params.get(id)
+
+
+def _get_id(title: str):
+    """Format the ID of the element"""
+
+    # The title must be a string
+    if not isinstance(title, str):
+        raise DataPortalInputError("Parameter title must be a string")
+
+    # The title may only contain letters, numbers, space, underscore, and dash
+    if not re.match("^[A-Za-z0-9_- ]*$", title):
+        msg = "Parameter title may only contain numbers, letters, and minimal punctuation"
+        raise DataPortalInputError(msg)
+    
+    # The first character must be a letter
+    if not re.match("^[A-Za-z]$", title[:1]):
+        msg = "The first character must be a letter"
+        raise DataPortalInputError(msg)
+    
+    id = title.lower()
+    for chr in [' ', '-']:
+        id = id.replace(chr, '_')
+
+    return id
+
+
+def _get_param_type(default):
+    """Map the type of the default value to the form schema."""
+
+    param_type = {
+        bool: "boolean",
+        str: "string",
+        int: "integer",
+        float: "number"
+    }.get(type(default))
+
+    if param_type is None:
+        msg = f"Default value must be bool, str, int, or float (not {type(default)})"
+        raise DataPortalInputError(msg)
+
+    return param_type
