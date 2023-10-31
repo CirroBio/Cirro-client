fa693d4a1fb2db85dd3c58ca522d2791f76b1c21
Sam Minot <sminot@fredhutch.org>
2023-10-31 09:13:43 -0700

    Add prompt for enable_additional_checksum

--- cirro/cli/interactive/auth_args.py
+++ cirro/cli/interactive/auth_args.py
@@ -1,6 +1,6 @@
 from typing import Tuple, Dict
 
-from cirro.cli.interactive.utils import ask_yes_no
+from cirro.cli.interactive.utils import ask_yes_no, ask
 
 
 def gather_auth_config() -> Tuple[str, Dict]:
@@ -8,4 +8,10 @@ def gather_auth_config() -> Tuple[str, Dict]:
         'enable_cache': ask_yes_no("Would you like to save your login? (do not use this on shared devices)")
     }
 
-    return 'ClientAuth', auth_method_config
+    enable_additional_checksum = ask(
+        "select",
+        "Upload / download validation type (note: SHA-256 requires additional local compute)",
+        choices=["MD5", "SHA-256"]
+    ) == "SHA-256"
+
+    return 'ClientAuth', auth_method_config, enable_additional_checksum
