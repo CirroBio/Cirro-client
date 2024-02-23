36b40a1627a1a624957fe89a151662dd89b205ff
Sam Minot <sminot@fredhutch.org>
2024-02-23 11:19:08 -0800

    Metadata methods docstrings

--- cirro/services/metadata.py
+++ cirro/services/metadata.py
@@ -2,7 +2,7 @@ from typing import List
 
 from cirro_api_client.v1.api.metadata import get_project_samples, get_project_schema, update_project_schema, \
     update_sample
-from cirro_api_client.v1.models import FormSchema, SampleRequest, Sample
+from cirro_api_client.v1.models import FormSchema, SampleRequest, Sample, FormSchema
 
 from cirro.services.base import BaseService, get_all_records
 
@@ -15,6 +15,9 @@ class MetadataService(BaseService):
         Args:
             project_id (str): ID of the Project
             max_items (int): Maximum number of records to get (default 10,000)
+
+        Returns:
+            `typing.List[cirro_api_client.v1.models.Sample]`
         """
         return get_all_records(
             records_getter=lambda page_args: get_project_samples.sync(project_id=project_id,
@@ -24,20 +27,41 @@ class MetadataService(BaseService):
             max_items=max_items
         )
 
-    def get_project_schema(self, project_id: str):
+    def get_project_schema(self, project_id: str) -> FormSchema:
         """
         Get project metadata schema
+
+        Args:
+            project_id (str): ID of the Project
+
         """
         return get_project_schema.sync(project_id=project_id, client=self._api_client)
 
     def update_project_schema(self, project_id: str, schema: FormSchema):
         """
         Update project metadata schema
+
+        Args:
+            project_id (str): ID of the Project
+            schema (cirro_api_client.v1.models.FormSchema): Metadata schema
         """
         update_project_schema.sync_detailed(project_id=project_id, body=schema, client=self._api_client)
 
-    def update_sample(self, project_id: str, sample_id: str, sample: SampleRequest):
+    def update_sample(self, project_id: str, sample_id: str, sample: SampleRequest) -> Sample:
         """
-        Updates metadata on a sample
+        Updates metadata information for sample
+
+        Args:
+            project_id (str): ID of the Project
+            sample_id (str): ID of the sample
+            sample (cirro_api_client.v1.models.SampleRequest): Metadata information for the sample
+
+        Returns:
+            `cirro_api_client.v1.models.Sample`: Updated sample metadata
         """
-        return update_sample.sync(project_id=project_id, sample_id=sample_id, body=sample, client=self._api_client)
+        return update_sample.sync(
+            project_id=project_id,
+            sample_id=sample_id,
+            body=sample,
+            client=self._api_client
+        )
