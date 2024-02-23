f1aeb403bfde5458bab5f3c95fe27e0f41cc1fa8
Sam Minot <sminot@fredhutch.org>
2024-02-23 11:14:30 -0800

    File method docstrings

--- cirro/services/file.py
+++ cirro/services/file.py
@@ -24,13 +24,21 @@ class FileService(BaseService):
     def get_access_credentials(self, access_context: FileAccessContext) -> AWSCredentials:
         """
         Retrieves credentials to access files
+
+        Args:
+            access_context (cirro.models.file.FileAccessContext): File access context, use class methods to generate
         """
         access_request = access_context.file_access_request
+
         if access_request.access_type == AccessType.PROJECT_DOWNLOAD:
             return self._get_project_read_credentials(access_context)
-        return generate_project_file_access_token.sync(client=self._api_client,
-                                                       project_id=access_context.project_id,
-                                                       body=access_context.file_access_request)
+
+        else:
+            return generate_project_file_access_token.sync(
+                client=self._api_client,
+                project_id=access_context.project_id,
+                body=access_context.file_access_request
+            )
 
     def _get_project_read_credentials(self, access_context: FileAccessContext):
         """
@@ -42,9 +50,12 @@ class FileService(BaseService):
             cached_token = self._read_token_cache.get(project_id)
 
             if not cached_token or datetime.now(tz=timezone.utc) > cached_token.expiration:
-                new_token = generate_project_file_access_token.sync(client=self._api_client,
-                                                                    project_id=project_id,
-                                                                    body=access_request)
+                new_token = generate_project_file_access_token.sync(
+                    client=self._api_client,
+                    project_id=project_id,
+                    body=access_request
+                )
+
                 self._read_token_cache[project_id] = new_token
 
         return self._read_token_cache[project_id]
@@ -52,53 +63,99 @@ class FileService(BaseService):
     def get_file(self, file: File) -> bytes:
         """
         Gets the contents of a file
+
+        Args:
+            file (cirro.models.file.File):
         """
         return self.get_file_from_path(file.access_context, file.relative_path)
 
     def get_file_from_path(self, access_context: FileAccessContext, file_path: str) -> bytes:
         """
         Gets the contents of a file by providing the path, used internally
+
+        Args:
+            access_context (cirro.models.file.FileAccessContext): File access context, use class methods to generate
+            file_path (str): Relative path to file within dataset
         """
-        s3_client = S3Client(partial(self.get_access_credentials, access_context),
-                             self.enable_additional_checksum)
+
+        s3_client = S3Client(
+            partial(self.get_access_credentials, access_context),
+            self.enable_additional_checksum
+        )
+
         full_path = f'{access_context.prefix}/{file_path}'.lstrip('/')
+
         return s3_client.get_file(access_context.bucket, full_path)
 
     def create_file(self, access_context: FileAccessContext, key: str,
                     contents: str, content_type: str):
         """
         Creates a file at the specified path
+
+        Args:
+            access_context (cirro.models.file.FileAccessContext): File access context, use class methods to generate
+            key (str): Key of object to create
+            contents (str): Content of object
+            content_type (str):
         """
-        s3_client = S3Client(partial(self.get_access_credentials, access_context),
-                             self.enable_additional_checksum)
-        s3_client.create_object(key=key,
-                                contents=contents,
-                                content_type=content_type,
-                                bucket=access_context.bucket)
+
+        s3_client = S3Client(
+            partial(self.get_access_credentials, access_context),
+            self.enable_additional_checksum
+        )
+
+        s3_client.create_object(
+            key=key,
+            contents=contents,
+            content_type=content_type,
+            bucket=access_context.bucket
+        )
 
     def upload_files(self, access_context: FileAccessContext, directory: str, files: List[str]):
         """
         Uploads a list of files from the specified directory
-        :param access_context: File access context, use class methods to generate
-        :param directory: base path to upload from
-        :param files: relative path of files to upload
-        :return:
+
+        Args:
+            access_context (cirro.models.file.FileAccessContext): File access context, use class methods to generate
+            directory (str): base path to upload from
+            files (List[str]): relative path of files to upload
         """
-        s3_client = S3Client(partial(self.get_access_credentials, access_context),
-                             self.enable_additional_checksum)
-        upload_directory(directory, files, s3_client, access_context.bucket, access_context.prefix,
-                         max_retries=self.transfer_retries)
+
+        s3_client = S3Client(
+            partial(self.get_access_credentials, access_context),
+            self.enable_additional_checksum
+        )
+
+        upload_directory(
+            directory,
+            files,
+            s3_client,
+            access_context.bucket,
+            access_context.prefix,
+            max_retries=self.transfer_retries
+        )
 
     def download_files(self, access_context: FileAccessContext, directory: str, files: List[str]):
         """
         Download a list of files to the specified directory
-        :param access_context: File access context, use class methods to generate
-        :param directory: download location
-        :param files: relative path of files to download
+
+        Args:
+            access_context (cirro.models.file.FileAccessContext): File access context, use class methods to generate
+            directory (str): download location
+            files (List[str]): relative path of files to download
         """
-        s3_client = S3Client(partial(self.get_access_credentials, access_context),
-                             self.enable_additional_checksum)
-        download_directory(directory, files, s3_client, access_context.bucket, access_context.prefix)
+        s3_client = S3Client(
+            partial(self.get_access_credentials, access_context),
+            self.enable_additional_checksum
+        )
+
+        download_directory(
+            directory,
+            files,
+            s3_client,
+            access_context.bucket,
+            access_context.prefix
+        )
 
 
 class FileEnabledService(BaseService):
