c6b3eaa5b36852b13af360998eb399f37fc1c215
Sam Minot <sminot@gmail.com>
2025-01-29 10:12:59 -0800

    Make a more convenient flow for authenticating the SDK

--- cirro/__init__.py
+++ cirro/__init__.py
@@ -2,12 +2,14 @@ import cirro.file_utils  # noqa
 from cirro.cirro_client import CirroApi
 from cirro.sdk.dataset import DataPortalDataset
 from cirro.sdk.portal import DataPortal
+from cirro.sdk.portal import DataPortalLogin
 from cirro.sdk.process import DataPortalProcess
 from cirro.sdk.project import DataPortalProject
 from cirro.sdk.reference import DataPortalReference
 
 __all__ = [
     'DataPortal',
+    'DataPortalLogin',
     'DataPortalProject',
     'DataPortalProcess',
     'DataPortalDataset',
