1c8856c91d79fc03ecc75dd4081b58799496b2f2
Sam Minot <sminot@fredhutch.org>
2025-01-30 10:57:08 -0800

    Enable DeviceCodeAuth without blocking for completion (#137)
    Co-authored-by: Nathan Thorpe <nathan@techstormpc.com>

--- cirro/__init__.py
+++ cirro/__init__.py
@@ -1,6 +1,7 @@
 import cirro.file_utils  # noqa
 from cirro.cirro_client import CirroApi
 from cirro.sdk.dataset import DataPortalDataset
+from cirro.sdk.login import DataPortalLogin
 from cirro.sdk.portal import DataPortal
 from cirro.sdk.process import DataPortalProcess
 from cirro.sdk.project import DataPortalProject
@@ -8,6 +9,7 @@ from cirro.sdk.reference import DataPortalReference
 
 __all__ = [
     'DataPortal',
+    'DataPortalLogin',
     'DataPortalProject',
     'DataPortalProcess',
     'DataPortalDataset',
--- cirro/auth/device_code.py
+++ cirro/auth/device_code.py
@@ -45,15 +45,15 @@ def _build_token_persistence(location: str, fallback_to_plaintext=False):
         return FilePersistence(location)
 
 
-def _authenticate(client_id: str, auth_endpoint: str, auth_io: Optional[StringIO] = None):
+def _initialize_auth_flow(client_id: str, auth_endpoint: str) -> DeviceTokenResponse:
     params = {'client_id': client_id}
     resp = requests.post(f'{auth_endpoint}/device-code', params=params)
     resp.raise_for_status()
     flow: DeviceTokenResponse = resp.json()
-    if auth_io is None:
-        print(flow['message'])
-    else:
-        auth_io.write(flow['message'])
+    return flow
+
+
+def _await_completion(client_id: str, auth_endpoint: str, flow: DeviceTokenResponse):
     device_expiry = datetime.fromisoformat(flow['expiry'])
 
     params = {
@@ -79,12 +79,32 @@ def _authenticate(client_id: str, auth_endpoint: str, auth_io: Optional[StringIO
     raise RuntimeError(f'error authenticating {auth_status}')
 
 
+def _authenticate(client_id: str, auth_endpoint: str, auth_io: Optional[StringIO] = None):
+    flow = _initialize_auth_flow(client_id=client_id, auth_endpoint=auth_endpoint)
+    if auth_io is None:
+        print(flow['message'])
+    else:
+        auth_io.write(flow['message'])
+    return _await_completion(client_id=client_id, auth_endpoint=auth_endpoint, flow=flow)
+
+
 class DeviceCodeAuth(AuthInfo):
     """
     Authenticates to Cirro by asking
-     the user to enter a verification code on the portal website
+    the user to enter a verification code on the portal website
 
+    :param client_id: The client ID for the OAuth application
+    :param region: The AWS region where the Cognito user pool is located
+    :param auth_endpoint: The endpoint for the OAuth authorization server
     :param enable_cache: Optionally enable cache to avoid re-authentication
+    :param auth_io: Optionally provide a StringIO object for the authentication link
+    :param await_completion:
+        If True, block until the user completes the authorization.
+            If auth_io is provided, the authorization message will be written to that buffer.
+            If auth_io is not provided, the authorization message will be printed.
+        If False, the object will be instantiated without fully completing the authorization.
+            The authorization message can be accessed using the .auth_message property.
+            Then, the await_completion() method must be run to complete the process.
 
     Implements the OAuth device code flow
     This is the preferred way to authenticate
@@ -95,12 +115,15 @@ class DeviceCodeAuth(AuthInfo):
         region: str,
         auth_endpoint: str,
         enable_cache=False,
-        auth_io: Optional[StringIO] = None
+        auth_io: Optional[StringIO] = None,
+        await_completion=True
     ):
         self.client_id = client_id
+        self.auth_endpoint = auth_endpoint
         self.region = region
         self._token_info: Optional[OAuthTokenResponse] = None
         self._persistence: Optional[BasePersistence] = None
+        self._flow: Optional[DeviceTokenResponse] = None
         self._token_path = Path(Constants.home, f'{client_id}.token.dat').expanduser()
 
         if enable_cache:
@@ -121,8 +144,48 @@ class DeviceCodeAuth(AuthInfo):
                 self._clear_token_info()
 
         if not self._token_info:
-            self._token_info = _authenticate(client_id=client_id, auth_endpoint=auth_endpoint, auth_io=auth_io)
+            if await_completion:
+                self._token_info = _authenticate(client_id=client_id, auth_endpoint=auth_endpoint, auth_io=auth_io)
+            else:
+                self._flow = _initialize_auth_flow(client_id=client_id, auth_endpoint=auth_endpoint)
 
+        if self._token_info:
+            self._save_token_info()
+            self._update_token_metadata()
+            self._get_token_lock = threading.Lock()
+
+    @property
+    def auth_message(self):
+        """
+        If the DeviceCodeAuth was instantiated with await_completion=False,
+        then the authorization message will be populated by this property.
+        """
+        if self._flow is None:
+            raise ValueError("The DeviceTokenResponse is not available")
+        else:
+            return self._flow["message"]
+
+    @property
+    def auth_message_markdown(self):
+        """
+        Markdown syntax for the authorization message, so that links are rendered appropriately.
+        """
+        return " ".join([
+            (
+                f"[{part}]({part})"
+                if part.startswith("http")
+                else part
+            )
+            for part in self.auth_message.split(" ")
+        ])
+
+    def await_completion(self):
+        """Block until the user completes the authorization process."""
+        self._token_info = _await_completion(
+            client_id=self.client_id,
+            auth_endpoint=self.auth_endpoint,
+            flow=self._flow
+        )
         self._save_token_info()
         self._update_token_metadata()
         self._get_token_lock = threading.Lock()
