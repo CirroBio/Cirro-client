f680c37d1c7f90fd859d27c3bbd9a425bc6833e8
Sam Minot <sminot@gmail.com>
2025-01-30 11:00:44 -0800

    Use relative_path

--- cirro/sdk/dataset.py
+++ cirro/sdk/dataset.py
@@ -141,12 +141,12 @@ class DataPortalDataset(DataPortalAsset):
             for i in ['name', 'id', 'description', 'status']
         ])
 
-    def get_file(self, relpath: str) -> DataPortalFile:
+    def get_file(self, relative_path: str) -> DataPortalFile:
         """
         Get a file from the dataset using its relative path.
 
         Args:
-            relpath (str): Relative path of file within the dataset
+            relative_path (str): Relative path of file within the dataset
 
         Returns:
             `from cirro.sdk.file import DataPortalFile`
@@ -157,16 +157,16 @@ class DataPortalDataset(DataPortalAsset):
 
         # Try getting the file using the relative path provided by the user
         try:
-            return files.get_by_id(relpath)
+            return files.get_by_id(relative_path)
         except DataPortalAssetNotFound:
             # Try getting the file with the 'data/' prefix prepended
             try:
-                return files.get_by_id("data/" + relpath)
+                return files.get_by_id("data/" + relative_path)
             except DataPortalAssetNotFound:
                 # If not found, raise the exception using the string provided
                 # by the user, not the data/ prepended version (which may be
                 # confusing to the user)
-                msg = '\n'.join([f"No file found with path '{relpath}'."])
+                msg = '\n'.join([f"No file found with path '{relative_path}'."])
                 raise DataPortalAssetNotFound(msg)
 
     def list_files(self) -> DataPortalFiles:
