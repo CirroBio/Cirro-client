a3e98ded76dc0f2353bda9f46e947e3707f12c52
Sam Minot <sminot@fredhutch.org>
2025-02-05 14:33:20 -0800

    CI-424 - Patch libraries for compatibility within pyodide (#141)
    ---------
    
    Co-authored-by: Nathan Thorpe <nathan@techstormpc.com>

---
+++ cirro/helpers/pyodide_patch_file.py
@@ -0,0 +1,47 @@
+import requests
+
+from cirro.models.file import FileAccessContext
+from cirro.services import FileService
+
+
+def _get_file_from_path(self: FileService, access_context: FileAccessContext, file_path: str):
+    # Load lazily -- must be installed with the pyodide extras
+    try:
+        from requests_aws4auth import AWS4Auth # noqa
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError("Could not load requests_aws4auth -- make sure to install cirro[pyodide]")
+
+    creds = self.get_access_credentials(access_context)
+
+    # Construct the URL endpoint to the file
+    endpoint = "/".join([
+        f"https://{access_context.bucket}.s3.{creds.region}.amazonaws.com",
+        access_context.prefix,
+        file_path
+    ])
+
+    # Set up the authorization
+    auth = AWS4Auth(
+        creds.access_key_id,
+        creds.secret_access_key,
+        creds.region,
+        's3',
+        session_token=creds.session_token,
+    )
+
+    # Make the request
+    response = requests.get(endpoint, auth=auth)
+
+    # Raise an error if the exit code is not 400
+    response.raise_for_status()
+
+    # Return the contents of the response
+    return response.content
+
+
+def pyodide_patch_file():
+    """
+    Patch the FileService to use requests to get files from S3,
+     rather than S3Client (boto3)
+    """
+    FileService.get_file_from_path = _get_file_from_path
---
+++ cirro/helpers/pyodide_patch_httpx.py
@@ -0,0 +1,72 @@
+import httpx
+import requests
+from httpx import Headers, Request, Response, Auth
+from httpx._client import Client, logger
+from requests.auth import AuthBase
+
+
+class HttpxToRequestsAuth(AuthBase):
+    def __init__(self, httpx_auth):
+        self.httpx_auth: Auth = httpx_auth
+
+    def __call__(self, request: Request):
+
+        # Create a mock HTTPX request
+        httpx_request = httpx.Request(
+            method=request.method,
+            url=request.url,
+            headers=request.headers
+        )
+
+        # Apply the httpx authentication
+        if self.httpx_auth:
+            self.httpx_auth.auth_flow(httpx_request)
+
+        # Copy back modified headers to the requests request
+        request.headers.update(httpx_request.headers)
+
+        return request
+
+
+def _send_single_request(self: Client, request: Request) -> Response:
+    auth_patched = HttpxToRequestsAuth(self.auth)
+
+    # Send the request using requests
+    resp = requests.request(
+        method=request.method,
+        url=request.url,
+        headers=request.headers,
+        data=request.content,
+        auth=auth_patched,
+    )
+
+    # Remove content-encoding header to prevent httpx from trying to decode the content
+    # since requests already decodes the content
+    if "content-encoding" in resp.headers:
+        del resp.headers["content-encoding"]
+
+    # Transform the requests response to the httpx response
+    response = Response(
+        status_code=resp.status_code,
+        headers=Headers(resp.headers),
+        content=resp.content,
+        request=httpx.Request(method=resp.request.method, url=str(resp.url)),
+    )
+
+    logger.debug(
+        'HTTP Request: %s %s "%s %d %s"',
+        request.method,
+        request.url,
+        response.http_version,
+        response.status_code,
+        response.reason_phrase,
+    )
+
+    return response
+
+
+def pyodide_patch_httpx():
+    """
+    Patch the httpx.Client to use requests to send requests
+    """
+    Client._send_single_request = _send_single_request
