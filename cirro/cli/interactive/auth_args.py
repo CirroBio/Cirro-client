1a6200995850272e475c78f25da3b86273c4dcd5
Nathan Thorpe <nathan@techstormpc.com>
2025-06-19 08:39:06 -0700

    input -> text

--- cirro/cli/interactive/auth_args.py
+++ cirro/cli/interactive/auth_args.py
@@ -9,8 +9,8 @@ logger = logging.getLogger()
 
 def gather_auth_config() -> Tuple[str, str, Dict, bool]:
     base_url = ask(
-        'input',
-        'Enter the URL of the Cirro instance you\'d like to connect to'
+        'text',
+        'Enter the URL of the Cirro instance you\'d like to connect to:'
     )
     # Fix user-provided base URL, if necessary
     base_url = extract_base_url(base_url)
