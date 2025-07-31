a9a0fed436afeb30fa1d4b51faa5e9b1e4c7ddf9
Nathan Thorpe <nathan@techstormpc.com>
2025-07-31 16:34:57 -0700

    fix test

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -11,25 +11,25 @@ DATA_PATH = os.path.join(__location__, 'data')
 
 class TestPreprocess(unittest.TestCase):
     def test_wide_samplesheet_1(self):
-        ds = PreprocessDataset(DATA_PATH + '/example_data_1')
+        ds = PreprocessDataset.from_path(DATA_PATH + '/example_data_1')
         df = ds.wide_samplesheet()
 
         expected_df = pandas.read_csv(DATA_PATH + '/example_data_1/expected_wide.csv')
 
         self.assertEqual(
             df.sort_index(axis=1).to_csv(index=None),
-            expected_df.sort_index(axis=1).to_csv(index=None)
+            expected_df.sort_index(axis=1).to_csv(index=False)
         )
 
     def test_wide_samplesheet_2(self):
-        ds = PreprocessDataset(DATA_PATH + '/example_data_2')
+        ds = PreprocessDataset.from_path(DATA_PATH + '/example_data_2')
         df = ds.wide_samplesheet()
 
         expected_df = pandas.read_csv(DATA_PATH + '/example_data_2/expected_wide.csv')
 
         self.assertEqual(
             df.sort_index(axis=1).to_csv(index=None),
-            expected_df.sort_index(axis=1).to_csv(index=None)
+            expected_df.sort_index(axis=1).to_csv(index=False)
         )
 
 
